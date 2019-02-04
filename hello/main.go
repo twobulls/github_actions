150c03bbbcc3fb6bc3ec25ad1c734bb0feb975aa
Alex Bielen <alex.bielen@twobulls.com>
2019-02-04 14:49:23 -0500

    Revert "Add backend chapter to body."
    This reverts commit 4a5d329846fec2a72c942f5f5debde6985179dfb.

--- hello/main.go
+++ hello/main.go
@@ -20,7 +20,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Hello From Two Bulls Backend Chapter!",
+		"message": "Hello From Two Bulls!",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
